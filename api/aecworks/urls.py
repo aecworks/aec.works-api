e8b1eec5b8b17334fd7c0adae6ee6453db5359f5
gtalarico <gtalarico.dev@gmail.com>
2020-05-21 07:32:26 +0000

    Clean up urls

--- api/aecworks/urls.py
+++ api/aecworks/urls.py
@@ -1,24 +1,23 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
-    path("jet/", include("jet.urls", "jet")),
+    # Admin + Jet (Admin Skin)
     path("admin/", admin.site.urls),
-    # Allow Login in browseable API
+    path("jet/", include("jet.urls", "jet")),
+    # DRF Browsable Api Auth
     path("api-auth/", include("rest_framework.urls")),
-    # Djoser Auth URL
+    # Djoser Auth - users, token and jwt urls
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
-    # path("auth/", include("djoser.urls.authtoken")),
     # Apps
     path("community/", include("api.community.urls")),
     path("users/", include("api.users.urls")),
 ]
 
 if settings.DEBUG:
-    import debug_toolbar  # noqa
-
     urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
 
 if settings.SERVE_STATIC:
