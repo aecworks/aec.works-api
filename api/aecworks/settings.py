b5c75304f74cf8798fd8bcf0b096f7098784208f
gtalarico <gtalarico.dev@gmail.com>
2020-08-06 09:36:20 -0700

    Increase token life

--- api/aecworks/settings.py
+++ api/aecworks/settings.py
@@ -29,7 +29,8 @@ GITHUB_CLIENT_SECRET = config("GITHUB_CLIENT_SECRET")
 # JWT Config
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 # Auth
 AUTH_USER_MODEL = "users.User"
