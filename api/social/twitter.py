441a4bc57a7d17089cdf09870742c14d76ddc217
gtalarico <gtalarico.dev@gmail.com>
2020-09-16 00:11:07 -0700

    fix

--- api/social/twitter.py
+++ api/social/twitter.py
@@ -3,13 +3,15 @@ from django.conf import settings
 
 
 def get_tweepy_client():
-    auth = tweepy.AppAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
+    auth = tweepy.AppAuthHandler(
+        settings.OAUTH_TWITTER_API_KEY, settings.OAUTH_TWITTER_API_SECRET
+    )
     return tweepy.API(auth)
 
 
 def get_timeline(handle, num=5):
     client = get_tweepy_client()
     items = []
-    for item in client.Cursor(client.user_timeline, screen_name=handle).items(num):
+    for item in tweepy.Cursor(client.user_timeline, screen_name=handle).items(num):
         items.append(item._json)
     return items
