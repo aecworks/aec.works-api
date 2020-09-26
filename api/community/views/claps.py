fe53942d59863e97da428ddaa5bfa9ed45fbbfec
gtalarico <gtalarico.dev@gmail.com>
2020-09-26 00:01:24 -0700

    Profile claps endpoint

--- api/community/views/claps.py
+++ api/community/views/claps.py
@@ -4,26 +4,14 @@ from rest_framework import (
     permissions,
 )
 from rest_framework.response import Response
-
 from api.common.exceptions import ErrorsMixin
-from api.common.utils import inline_serializer
-from api.users.serializers import ProfileSerializer
-from .. import models, selectors
+from .. import selectors
 
 
-class CompanyClapResponseSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
-    company = inline_serializer(
-        fields={
-            "name": serializers.CharField(),
-            "slug": serializers.CharField(),
-            "logo_url": serializers.URLField(),
-        }
-    )
-
-    class Meta:
-        model = models.Comment
-        fields = ["profile", "company"]
+class CompanyClapResponseSerializer(serializers.Serializer):
+    name = serializers.CharField(source="company.name")
+    slug = serializers.CharField(source="company.slug")
+    logo_url = serializers.CharField(source="company.logo_url")
 
 
 class CompanyClapsListView(ErrorsMixin, generics.GenericAPIView):
@@ -32,10 +20,9 @@ class CompanyClapsListView(ErrorsMixin, generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        profile = self.request.query_params.get("profile")
-        qs = selectors.get_company_claps()
-        return qs if not profile else qs.filter(profile__slug=profile)
+        profile = self.kwargs["profile"]
+        return selectors.get_company_claps().filter(profile__slug=profile)
 
-    def get(self, request):
+    def get(self, request, profile):
         serializer = CompanyClapResponseSerializer(self.get_queryset(), many=True)
         return Response(serializer.data)
