3b6012030acda24756f84d508f369b844b074558
gtalarico <gtalarico.dev@gmail.com>
2020-10-03 00:42:51 -0700

    Add aws header

--- api/aecworks/settings.py
+++ api/aecworks/settings.py
@@ -171,6 +171,9 @@ AWS_S3_CUSTOM_DOMAIN = config("DJANGO_S3_DOMAIN", default=None)  # prod only
 AWS_S3_REGION_NAME = "us-west-1"
 AWS_DEFAULT_ACL = "public-read"
 AWS_QUERYSTRING_AUTH = False  # Remove Query Auth from Image Url
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
 
 # CELERY STUFF
 CELERY_BROKER_URL = config("REDIS_URL")
