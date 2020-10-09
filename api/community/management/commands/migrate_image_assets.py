02caafbf6899e5132a82ebf88fb781b3cf6bb5b7
Gui Talarico <gtalarico@users.noreply.github.com>
2020-10-08 21:29:01 -0700

    Mid step migration (#36)

---
+++ api/community/management/commands/migrate_image_assets.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from api.users.models import Profile
+from api.community.models import Company, CompanyRevision
+from api.images.models import ImageAsset
+from api.images.service import create_image_file_from_url
+
+
+class Command(BaseCommand):
+    help = "ImageAssetsFromUrl"
+
+    def handle(self, *args, **options):
+        """ Temporary Script to migrate from url images to ImageAsset """
+
+        all_companies = [
+            *list(Company.objects.all()),
+            *list(CompanyRevision.objects.all()),
+        ]
+        for obj in all_companies:
+            if obj.logo_url:
+                img_file = create_image_file_from_url(obj.logo_url)
+                img = ImageAsset.objects.create(file=img_file)
+                obj.logo = img
+                print(f"[{obj}] {img} for {obj.logo_url}")
+                obj.save()
+
+            if obj.cover_url:
+                img_file = create_image_file_from_url(obj.cover_url)
+                img = ImageAsset.objects.create(file=img_file)
+                obj.cover = img
+                print(f"[{obj}] {img} for {obj.cover_url}")
+                obj.save()
+        for obj in Profile.objects.all():
+            if obj.avatar_url:
+                img_file = create_image_file_from_url(obj.avatar_url)
+                img = ImageAsset.objects.create(file=img_file)
+                obj.avatar = img
+                print(f"[{obj}] {img} for {obj.avatar_url}")
+                obj.save()
