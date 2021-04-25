dba71130f312b2f08e5d5443412c1e41b59a9573
gtalarico <gtalarico.dev@gmail.com>
2021-04-24 19:39:59 -0700

    Fixes 81

--- api/aecworks/urls.py
+++ api/aecworks/urls.py
@@ -1,4 +1,6 @@
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -20,3 +22,6 @@ urlpatterns = [
     # Debug Toolbar
     path("__debug__/", include(debug_toolbar.urls)),
 ]
+
+if settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
