9d6d63f6ab2a72a6de436aab3565d692b5bb8ec7
gtalarico <gtalarico.dev@gmail.com>
2023-01-20 08:08:48 -0800

    remove celery

--- api/celery_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.aecworks.settings")
-celery_app = Celery("api")
-celery_app.config_from_object("django.conf:settings", namespace="CELERY")
-celery_app.autodiscover_tasks()
